88d42148969e82d2affd549508bbd90e96789fb2
Taj-Hakeem Ryles (He/Him) <69494760+t-ryles@users.noreply.github.com>
2021-10-10 23:08:50 -0500

    Weather API
    Takes in a user zip code and returns the weather information for that area.

---
+++ API/app.js
@@ -0,0 +1,37 @@
+console.log("About to fetch weather data.");
+
+apiKey = ""
+units = "imperial"
+
+
+submitBTN = document.getElementById("btn");
+
+submitBTN.addEventListener('click', weatherData => {
+
+    zipcode = document.getElementById("zipCode").value;
+    imgDiv = document.getElementById("imgDiv");
+
+    async function weatherData() {
+
+        const response = await fetch("https://api.openweathermap.org/data/2.5/weather?zip="+zipcode+"&&units="+units+"&appid="+apiKey);
+        //Checking the url response status
+        //The await key word is waiting for the response from the fetch function, then converting it into JSON data. 
+        const data = await response.json();
+        //respose.json returns a promise, so we have to await that response.
+        const img = document.createElement("img");
+        const imgCode = data.weather[0].icon;
+        //console.log(imgCode);
+        img.src = `http://openweathermap.org/img/wn/${imgCode}@2x.png`;
+        //console.log(img.src);
+        imgDiv.append(img);
+
+        document.getElementById("city").append(" " + data.name);
+        document.getElementById("temp").append(" " + data.main.temp);
+        document.getElementById("feels").append(" " + data.main.feels_like);
+    }
+    
+    weatherData().catch(error => {
+        console.log('Error!')
+        console.log(error)
+    });
+})
